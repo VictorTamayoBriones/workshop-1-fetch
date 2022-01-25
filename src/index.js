038b5bac4f150f8c6cbaf8ef9add4dadd254ae50
VictorTamayoBriones <victortamayo509@gmail.com>
2022-01-25 00:31:02 -0600

    fisrt

--- src/index.js
+++ src/index.js
@@ -1,6 +1,38 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+const apiUrl = "https://platzi-avo.vercel.app/api/avo";
 
-console.log('Happy hacking :)')
+const createContainer = ()=>{
+    const container = document.createElement('div');
+    container.setAttribute('class', 'avocados');
+    document.body.appendChild(container);
+}
+
+const createCard = (img, name, price) =>{
+    const card = document.createElement('div');
+    const image = document.createElement('img');
+    const title = document.createElement('h2');
+    const avocadoPrice = document.createElement('p');
+    
+    card.setAttribute('class', 'card');
+    image.setAttribute('src', img);
+    
+    title.textContent(name);
+    avocadoPrice.textContent(price);
+    
+    card.append(image, title, avocadoPrice);
+    document.body.appendChild(card);   
+}
+
+//conectarnos al server
+window.fetch(apiUrl)
+    //Procesar la respuesta
+    .then((res)=>res.json())
+    //crear un container de avocados
+    // createContainer()
+
+    //JSON -> DATA -> RENDERIZAR LA INFO
+    .then((resJson)=>{
+        resJson.data.forEach(({name, image, price}) => {
+            //Crear una card con la imagen, titulo y precio
+            createCard(image, name, price);
+        });
+    })
